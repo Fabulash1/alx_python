16825300d0980550d31c45c433b6f9c262916b94
Fabulash1 <wizzlin1993@gmail.com>
2023-07-25 22:28:43 +0100

    Task 1

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Import a simple function"""
+
+from add_0 import add
+
+a = 1
+b = 2
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    pass
