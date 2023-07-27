4e184e00461cd603c72c273715057993640e3d93
Fabulash1 <wizzlin1993@gmail.com>
2023-07-27 13:29:41 +0100

    Task 0: square matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
     for p in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+        new_matrix[p] = list(map(lambda x: x**2, matrix[p]))
 
     return (new_matrix)
