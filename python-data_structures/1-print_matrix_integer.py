0e3f2d32accb2d148590d9383d744c6e52138404
Fabulash1 <wizzlin1993@gmail.com>
2023-07-27 13:07:08 +0100

    Task 3: returns

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,2 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
-    int = ("")
+    for row in matrix:
+            for col in row:
+                print("{:d}".format(col), end=" " if col != row[-1] else ""):
+                print()
