19024304ccd6b5f586eeaf88e2512342b85894ed
Fabulash1 <wizzlin1993@gmail.com>
2023-08-02 22:00:59 +0100

    Task 7

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -8,11 +8,12 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Intialize a new Rectangle.
-
+        
         Args:
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
         """
+        
         super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
