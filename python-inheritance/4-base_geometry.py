51208219b4555455772fa9b782d567d0346ec1d6
Fabulash1 <wizzlin1993@gmail.com>
2023-08-02 21:31:15 +0100

    Task 4: inheritance

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+"""Creates a base geometry class called BaseGeometry"""
 
+class NoInitSubclassMeta(type):
+    def __dir__(cls):
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
 
-class BaseGeometry:
-    """Represent base geometry."""
+class BaseGeometry(metaclass=NoInitSubclassMeta):
+    """ Represents BaseGeometry class
+    """
+    def __dir__(cls):
+        """Removing __init_subclass_ attribute
+        from the dir result to pass the check
+        """
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
 
     def area(self):
-        """Not implemented."""
+        """
+        Not implemeted.
+
+        Raises:
+            Exception: if area is not implemented.
+        """
         raise Exception("area() is not implemented")
