8a314f1a56d5dcc3b6d37de75c011e310c6b5215
Fabulash1 <wizzlin1993@gmail.com>
2023-08-12 20:35:35 +0100

    edited

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ def send_post_request(url, email):
     response = requests.post(url, data=data)
     return response.text
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     response_body = send_post_request(url, email)
