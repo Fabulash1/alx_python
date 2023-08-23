423d04405f2a4604a32e2624e79980c4be1cfe7a
Fabulash1 <wizzlin1993@gmail.com>
2023-08-23 16:45:22 +0100

    Tasks: python ORM

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # Write a script that lists all states from the database hbtn_0e_0_usa:
-"""lists all states 
-from the database hbtn_0e_0_usa"""
+"""lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
 
